f4cad20098f00d862e69754bd139e7b6b02cf9ce
Pratyush Kaushik <pratyush.kaushik@afry.com>
2024-08-20 12:23:06 +0530

    LED.py modified

--- LED.py
+++ LED.py
@@ -5,7 +5,7 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-pinout = 14
+pinout = 15
 color = "Yellow"
 GPIO.setup(pinout,GPIO.OUT)
 print ("LED on N" + str(pinout) + " " + color)
