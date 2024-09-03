0bd7b362e1dabc68fa9eeae30f1114ffeac41f9c
PratyushKaus <81761367+PratyushKaus@users.noreply.github.com>
2024-09-03 15:23:34 +0530

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,2 +1,14 @@
+#Led.py 
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-print("Hello world")
+pinout = 18
+color = "Yellow"
+GPIO.setup(pinout,GPIO.OUT)
+print ("LED on N" + str(pinout) + " " + color)
+GPIO.output(pinout,GPIO.HIGH)
+time.sleep(1)
+print ("LED off N" + str(pinout) + " " + color)
+GPIO.output(pinout,GPIO.LOW)
